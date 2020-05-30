156b366c480251223ff1f41a3309826aa724129a
Tomasz Skoczewski <tomaszskoczewski@gmail.com>
2020-05-31 00:46:03 +0200

    listItem update

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,6 +1,20 @@
 import React from "react";
+import danAbramovImage from "../../../assets/images/danabramov.jpg";
 import "./ListItem.css";
 
-const ListItem = () => <li className="listItemWrapper">item1</li>;
+const ListItem = () => (
+  <li className="listItem__wrapper">
+    <img src={danAbramovImage} alt="danabramov" className="listItem__image" />
+    <div>
+      <h2 className="listItem__name">Dan Abramov</h2>
+      <p className="listItem__description">
+        > It is a good idea to think of your PC as an office. It stores files,
+        programs, pictures. This can be compared to an actual office's files,
+        machines and decorations.
+      </p>
+      <button className="listItem__button">visit twitter page</button>
+    </div>
+  </li>
+);
 
 export default ListItem;
